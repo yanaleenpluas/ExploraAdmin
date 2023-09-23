7f93eff5da148bd321dc0a30d4fd776c4740a279
Alexander Goussas <goussasalexander@gmail.com>
2023-09-23 12:10:43 -0500

    update scss

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,6 +3,7 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
+    h1 "Hola, #{current_admin_user.email}"
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
         span I18n.t("active_admin.dashboard_welcome.welcome")
