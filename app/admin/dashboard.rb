379d3e2f2659ea54addbe3bd7d2c45c9244f0008
Alexander Goussas <goussasalexander@gmail.com>
2023-09-23 15:36:32 -0500

    make dashboard not appear on the menu

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 ActiveAdmin.register_page "Dashboard" do
-  menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
+  menu false
 
   content title: proc { I18n.t("active_admin.dashboard") } do
     h1 "Hola, #{current_admin_user.name} #{current_admin_user.last_name}"
