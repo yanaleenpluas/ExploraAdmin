e97a37e8eaa2da09600dd79e61eb334acaf943d1
Alexander Goussas <goussasalexander@gmail.com>
2023-09-25 10:12:52 -0500

    add doc comment to DashboardController::set_selected_institution

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,14 @@
 class DashboardController < ApplicationController
   protect_from_forgery except: [:set_selected_institution]
 
+  # Sets the selected institution as session data to be used by other pages.
+  #
+  # A redirect to the next step in the course creation process is performed, in
+  # this case that would be the page for choosing a term to work on.
   def set_selected_institution 
     institution = params[:institution]
     session[:institution] = institution
+
     # TODO: Redirect to la pagina de washington.
     redirect_to admin_dashboard_path
   end
